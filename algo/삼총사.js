276570bf676c3601341f3589938abd0c933e13ad
kiminn <134191815+kiminn@users.noreply.github.com>
2024-03-01 17:01:49 +0900

    0301-algo-삼총사

---
+++ algo/삼총사.js
@@ -0,0 +1,28 @@
+function solution(number) {
+    let answer = 0;
+    for (i = 0; i < number.length; i++) {
+        for (j = i + 1; j < number.length; j++) {
+            for (k = j + 1; k < number.length; k++) {
+                if (number[i] + number[j] + number[k] === 0) return answer++;
+            }
+        }
+    }
+
+    return answer;
+}
+
+/*
+return 삼총사를 만들 수 있는 방법의 수
+
+3명의 정수번호 더했을 때 0이되면 삼총사
+
+0 1 2
+0 1 3
+0 1 4
+0 2 3
+0 2 4
+.
+.
+.
+
+*/
