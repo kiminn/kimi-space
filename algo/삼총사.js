97fbac1c7f1e6c4d2067f2bb1ec68aad8f53acb7
kiminn <134191815+kiminn@users.noreply.github.com>
2024-03-01 17:02:31 +0900

    0301-algo-삼총사

--- algo/삼총사.js
+++ algo/삼총사.js
@@ -3,7 +3,7 @@ function solution(number) {
     for (i = 0; i < number.length; i++) {
         for (j = i + 1; j < number.length; j++) {
             for (k = j + 1; k < number.length; k++) {
-                if (number[i] + number[j] + number[k] === 0) return answer++;
+                if (number[i] + number[j] + number[k] === 0) answer++;
             }
         }
     }
